8c7c19f3e2ff2e6fa559d03d89a26e53f31290b4
onaeem <onaeem@creativechaos.co>
2017-09-22 20:14:44 +0500

    heroku update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+
+  def index
+    @content_first = 'This is some sample paragraph for ruby Blog hurray'
+    @content_second = 'This is more sample paragraph for ruby Blog'
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
